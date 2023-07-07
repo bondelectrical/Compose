618f59c486521f9b2c82ea60ca10c4d12b5cc2f6
Bondarenko <aleksejbondarenko18@gmail.com>
2023-07-07 11:07:53 +0300

    added constructor

--- app/src/main/java/net/ucoz/testcompose/presentation/jobList/JobListViewModel.kt
+++ app/src/main/java/net/ucoz/testcompose/presentation/jobList/JobListViewModel.kt
@@ -1,12 +1,59 @@
 package net.ucoz.testcompose.presentation.jobList
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import net.ucoz.testcompose.domain.common.NetworkResult
+import net.ucoz.testcompose.domain.interactor.JobInteractor
 import net.ucoz.testcompose.presentation.base.BaseViewModel
+import net.ucoz.testcompose.presentation.models.JobUI
+import javax.inject.Inject
 
-class JobListViewModel :
+@HiltViewModel
+class JobListViewModel  @Inject constructor(
+    private val interactor: JobInteractor,
+) :
     BaseViewModel<
             JobListContract.Event,
             JobListContract.State,
             JobListContract.Effect>() {
+
+    init {
+        viewModelScope.launch {
+            setState { copy(isLoading = true, isError = false) }
+            when (val networkResult = interactor.getJobList()) {
+                is NetworkResult.SuccessWithData -> {
+                    Log.d("MyTag", networkResult.data[0].pauseName)
+                    setState {
+                        copy(
+                            isLoading = false,
+                            isError = false,
+                            jobList = networkResult.data.map { JobUI(it) })
+                    }
+                }
+
+                is NetworkResult.ServerError -> {
+                    setState {
+                        copy(
+                            isLoading = false, isError = true, errorMessage = networkResult.message
+                        )
+                    }
+
+                }
+
+                is NetworkResult.Error -> {
+                    setState {
+                        copy(
+                            isLoading = false,
+                            isError = true,
+                            errorMessage = networkResult.exception?.message
+                        )
+                    }
+                }
+            }
+        }
+    }
 
     override fun setInitialState() = JobListContract.State()
 
